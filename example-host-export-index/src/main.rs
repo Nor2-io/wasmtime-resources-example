de52751561c91ecf8ddc4d0f13dc9af5681d61a5
Timmy Silesmo <silesmo@nor2.io>
2023-08-22 22:31:39 +0200

    resources basic working example

--- example-host-export-index/src/main.rs
+++ example-host-export-index/src/main.rs
@@ -1,23 +1,17 @@
 use anyhow::anyhow;
-use wasmtime::component::*;
+use wasmtime::{component::*, AsContextMut};
 use wasmtime::{Config, Engine, Store};
 
-//TODO: Change resources from hashmap to allow for more than one implementation of the same resource type
-bindgen!({
-    path: "./example2.wit",
-    resources: {
-        "scalars": ImplScalars
-    }
-});
+use crate::example2::component::backend::Scalars;
 
-pub trait ResourceDrop<T> {
-    fn drop(store: &mut StoreContextMut<'_, T>, rep: u32) -> wasmtime::Result<()>;
-}
+bindgen!({
+    path: "./example2.wit"
+});
 
 #[derive(Debug, Default)]
 pub struct Host;
 
-impl crate::example2::component::backend::Host for Host {
+impl example2::component::backend::Host for Host {
     fn fetch(&mut self, _url: String) -> wasmtime::Result<Vec<u8>> {
         Ok(Vec::new())
     }
@@ -26,37 +20,49 @@ impl crate::example2::component::backend::Host for Host {
 #[derive(Debug, Default)]
 pub struct State {
     host: Host,
-    scalars: Option<ImplScalars>,
+    scalars_table: std::collections::HashMap<u32, ImplScalars>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ImplScalars;
 
-impl<T> ResourceDrop<T> for ImplScalars {
-    fn drop(store: &mut wasmtime::StoreContextMut<'_, T>, rep: u32) -> wasmtime::Result<()> {
-        //TODO: Don't we need the the generic in Store<T> to implement a trait to be able to access the data and drop the handle here?
+impl example2::component::backend::Scalars for State {
+    type Resource = ImplScalars;
 
-        Ok(())
-    }
-}
-
-impl crate::example2::component::backend::Scalars for ImplScalars {
-    fn new<T>(
-        _store: &mut wasmtime::StoreContextMut<'_, T>,
-    ) -> wasmtime::Result<(Self, wasmtime::component::Resource<Self>)>
+    fn new(
+        store: &mut wasmtime::StoreContextMut<'_, Self>,
+    ) -> wasmtime::Result<wasmtime::component::Resource<Self::Resource>>
     where
         Self: Sized,
     {
-        Ok((Self, wasmtime::component::Resource::<Self>::new_own(1)))
+        let state = store.data_mut();
+
+        let handle =
+            wasmtime::component::Resource::<ImplScalars>::new_own(state.scalars_table.len() as u32);
+        state.scalars_table.insert(handle.rep(), ImplScalars);
+
+        Ok(handle)
     }
+
     fn method_scalars_get_b(
-        &self,
-        _self_: wasmtime::component::Resource<Self>,
+        &mut self,
+        _self_: wasmtime::component::Resource<Self::Resource>,
     ) -> wasmtime::Result<u32>
     where
         Self: Sized,
     {
         Ok(5)
+    }
+
+    fn drop(mut store: wasmtime::StoreContextMut<'_, Self>, rep: u32) -> wasmtime::Result<()> {
+        let state = store.data_mut();
+
+        state.scalars_table.remove(&rep).ok_or(anyhow!(
+            "tried to drop a resource `{}` that doesn't exist",
+            rep
+        ))?;
+
+        Ok(())
     }
 }
 
@@ -69,27 +75,20 @@ async fn main() -> wasmtime::Result<()> {
     let component = Component::from_file(&engine, "./example_guest_import.wasm").unwrap();
     let mut linker = Linker::new(&engine);
 
-    let mut store = Store::new(&engine, State::default());
+    let state = State::default();
 
-    crate::example2::component::backend::add_to_linker::<State, ImplScalars, Host>(
-        &mut linker,
-        |s| &mut s.host,
-        |s, rep| {
-            //In a real implementetion you would lookup the resource based on the argument `rep`
-            let resource = std::mem::take(&mut s.scalars);
-            resource.unwrap()
-        },
-    )?;
+    let mut store = Store::new(&engine, state);
+    example2::component::backend::add_to_linker::<State, Host>(&mut linker, |s| &mut s.host)?;
 
     let (bindings, _instance) = Example2::instantiate(&mut store, &component, &linker).unwrap();
 
+    let resource = State::new(&mut store.as_context_mut())?;
+
     let test = bindings
         .example2_component_front()
-        .call_handle(
-            &mut store,
-            wasmtime::component::Resource::<ImplScalars>::new_own(1),
-        )
+        .call_handle(&mut store, resource)
         .unwrap();
+
     dbg!(test);
 
     Ok(())
